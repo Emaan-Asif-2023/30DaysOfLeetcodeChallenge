e72e21709ec9d968d27a7533dcb0cc1d1c51cea9
Emaan-Asif-2023 <136973601+Emaan-Asif-2023@users.noreply.github.com>
2024-03-21 00:11:41 +0500

    Time: 362 ms (5.02%) | Memory: 20.4 MB (55.48%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> merged;
         int i = 0;
-        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-            merged.push_back(intervals[i]);
-            i++;
+        
+        while(i < intervals.size()) {
+            if(intervals[i][1] < newInterval[0])
+                i++;
+            else if (intervals[i][0] > newInterval[1]){
+                intervals.insert(intervals.begin() + i, newInterval);
+                return intervals;
+            } else {
+               
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+                intervals.erase(intervals.begin()+i);
+            }
         }
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        merged.push_back(newInterval);
-    
-    
-        while (i < intervals.size()) {
-            merged.push_back(intervals[i]);
-            i++;
-        }
-    
-        return merged;
+        
+        intervals.push_back(newInterval);
+        return intervals;
     }
-    
     
 };
